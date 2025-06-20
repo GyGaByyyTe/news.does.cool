4beded11ae364604d9a3a27bd418742d6abd07a6
Andrei Lebedev <gygabyyyte@gmail.com>
2025-06-20 12:20:47 +0200

    Close modal on backdrop

--- app/news/[id]/@modal/(.)image/page.tsx
+++ app/news/[id]/@modal/(.)image/page.tsx
@@ -1,12 +1,11 @@
+'use client';
+import React from 'react';
 import { NEWS_DB } from '@/news-db';
-import { notFound } from 'next/navigation';
+import { notFound, useRouter, useParams } from 'next/navigation';
 
-export default async function InterceptedImagePage({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
-  const { id: newsSlug } = await params;
+export default function InterceptedImagePage() {
+  const { id: newsSlug } = useParams();
+  const router = useRouter();
   const newsItem = NEWS_DB.find((item) => item.slug === newsSlug);
 
   if (!newsItem) {
@@ -15,7 +14,7 @@ export default async function InterceptedImagePage({
 
   return (
     <>
-      <div className="modal-backdrop" />
+      <div className="modal-backdrop" onClick={router.back} />
       <dialog open className="modal">
         <div className="fullscreen-image">
           <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
